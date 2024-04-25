cec7c067c4c6c8cd83d6fb376005e38a53d77254
Gerson Ribeiro <gerson.paula@ciandt.com>
2024-04-25 12:03:04 -0300

    adjusting files

--- src/core/functions/utils/getTemplateForWebview/index.ts
+++ src/core/functions/utils/getTemplateForWebview/index.ts
@@ -8,14 +8,19 @@ export default async (
 ): Promise<string> => {
   const { htmlPath, cssPath, jsPath } = chat(_extensionUri);
 
-  const htmlTxt = fs.readFileSync(htmlPath.fsPath, "utf-8");
-  const cssWebview = webviewView.webview.asWebviewUri(cssPath);
-  const jsWebview = webviewView.webview.asWebviewUri(jsPath);
+  if (fs.existsSync(htmlPath.fsPath)) {
+    const htmlTxt = fs.readFileSync(htmlPath.fsPath, "utf-8");
+    const cssWebview = webviewView.webview.asWebviewUri(cssPath);
+    const jsWebview = webviewView.webview.asWebviewUri(jsPath);
 
-  const htmlReplacedContext = htmlTxt
-    .replace("{{styles}}", cssWebview.toString())
-    .replace("{{jsPath}}", jsWebview.toString())
-    .replace("{{cspSource}}", webviewView.webview.cspSource);
+    const htmlReplacedContext = htmlTxt
+      .replace("{{styles}}", cssWebview.toString())
+      .replace("{{jsPath}}", jsWebview.toString())
+      .replace("{{cspSource}}", webviewView.webview.cspSource);
 
-  return htmlReplacedContext;
+    return htmlReplacedContext;
+  } else {
+    console.error("File does not exist: " + htmlPath.toString());
+    return "File does not exist: " + htmlPath.toString();
+  }
 };
